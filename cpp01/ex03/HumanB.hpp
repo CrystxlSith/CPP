fd1499a5d8c66a6d626c538ae6b7b2323f85beeb
CrystalSith <joaquim1810@gmail.com>
2024-10-21 08:24:19 +0200

    violence

--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -1,16 +1,18 @@
 #ifndef HUMANB_HPP
 # define HUMANB_HPP
 
+#include "Weapon.hpp"
+
 class HumanB
 {
 private:
-    /* data */
+    std::string _name;
+    Weapon* _weapon;
 public:
-    HumanB(/* args */);
+    HumanB( std::string name );
     ~HumanB();
+    void	attack();
+	void	setWeapon( Weapon& weapon);
 };
-
-
-
 
 #endif
