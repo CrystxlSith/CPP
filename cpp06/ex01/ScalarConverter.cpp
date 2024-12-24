07841fec242d26b30309a74d3b44b49331eda6dc
crytalsith <crystalsithh@gmail.com>
2024-12-24 18:07:57 +0100

    init ex00 cpp06

---
+++ cpp06/ex01/ScalarConverter.cpp
@@ -0,0 +1,11 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(/* args */)
+{
+    std::cout << "{1} ScalarConvertor Constructor Called" << std::endl;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+    std::cout << "{1} ScalarConvertor Destructor Called" << std::endl;
+}
---
+++ cpp06/ex01/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+class ScalarConverter
+{
+private:
+    ScalarConverter(/* args */);
+    ~ScalarConverter();
+public:
+    static void * convert(void *);
+};
+
+
